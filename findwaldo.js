85fdc5fa4b795f0e76b0e6984a392a42149e7438
Henry <mbahenryemeka@gmail.com>
2024-05-01 07:24:06 -0400

    Iteration using forEach

--- findwaldo.js
+++ findwaldo.js
@@ -1,12 +1,12 @@
 const newFunction = function (names, found) {
-  names.forEach(function (value, index){
+  names.forEach(function (value, index){  //get the value of iteration and index
     if(value === "Waldo"){
-      found(value, index)
+      found(value, index)  //callback when value waldo is found
     }
   })  
   }
-  const actionWhenFound = function (name, i) {
-    console.log("found " + name + " at index " + i + "!");
+  const actionWhenFound = function (value, index) {
+    console.log("found " + value + " at index " + index + "!");
   };
   
   newFunction(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
